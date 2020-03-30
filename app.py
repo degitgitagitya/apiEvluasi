e68ae908dd3ede88bfe60bf95724b252700716bd
degitgitagitya <degitgitagitya@gmail.com>
2020-03-30 14:22:34 +0700

    update model

--- app.py
+++ app.py
@@ -30,19 +30,22 @@ class Siswa(db.Model):
     nama = db.Column(db.String(100))
     jenis_kelamin = db.Column(db.String(100))
     password = db.Column(db.String(100))
+    nilai_pretest = db.Column(db.Integer)
 
-    def __init__(self, id_kelas, nis, nama, jenis_kelamin, password):
+    def __init__(self, id_kelas, nis, nama, jenis_kelamin, password, nilai_pretest):
         self.id_kelas = id_kelas
         self.nis = nis
         self.nama = nama
         self.jenis_kelamin = jenis_kelamin
         self.password = password
+        self.nilai_pretest = nilai_pretest
 
 
 # Siswa Schema
 class SiswaSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'id_kelas', 'nis', 'nama', 'jenis_kelamin', 'password')
+        fields = ('id', 'id_kelas', 'nis', 'nama',
+                  'jenis_kelamin', 'password', 'nilai_pretest')
 
 
 # Init Siswa Schema
@@ -73,8 +76,10 @@ def add_siswa():
     nama = request.json['nama']
     jenis_kelamin = request.json['jenis_kelamin']
     password = request.json['password']
+    nilai_pretest = request.json['nilai_pretest']
 
-    new_siswa = Siswa(id_kelas, nis, nama, jenis_kelamin, password)
+    new_siswa = Siswa(id_kelas, nis, nama, jenis_kelamin,
+                      password, nilai_pretest)
     db.session.add(new_siswa)
     db.session.commit()
 
@@ -99,6 +104,7 @@ def update_siswa(id):
     siswa.nama = request.json['nama']
     siswa.jenis_kelamin = request.json['jenis_kelamin']
     siswa.password = request.json['password']
+    siswa.nilai_pretest = request.json['nilai_pretest']
 
     db.session.commit()
 
@@ -258,6 +264,140 @@ def update_kelas(id):
     db.session.commit()
 
     return kelas_schema.jsonify(kelas)
+
+
+# Model Pre Test
+class Test(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pertanyaan = db.Column(db.String(100))
+
+    def __init__(self, pertanyaan):
+        self.pertanyaan = pertanyaan
+
+
+# Pre Test Schema
+class TestSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'pertanyaan')
+
+
+# Init Test Schema
+test_schema = TestSchema()
+tests_schema = TestSchema(many=True)
+
+# Get All Test
+@app.route('/test', methods=['GET'])
+def get_all_test():
+    all_test = Test.query.all()
+    result = tests_schema.dump(all_test)
+
+    return jsonify(result)
+
+# Create a Test
+@app.route('/test', methods=['POST'])
+def add_test():
+    pertanyaan = request.json['pertanyaan']
+
+    new_test = Test(pertanyaan)
+    db.session.add(new_test)
+    db.session.commit()
+
+    return test_schema.jsonify(new_test)
+
+# Delete a Test
+@app.route('/test/<id>', methods=['DELETE'])
+def delete_test(id):
+    test = Test.query.get(id)
+    db.session.delete(test)
+    db.session.commit()
+
+    return test_schema.jsonify(test)
+
+# Edit a Test
+@app.route('/test/<id>', methods=['PUT'])
+def update_test(id):
+    test = Test.query.get(id)
+
+    test.pertanyaan = request.json['pertanyaan']
+
+    db.session.commit()
+
+    return test_schema.jsonify(test)
+
+
+# Model Pilihan Test
+class PilihanTest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_test = db.Column(db.Integer)
+    pilihan = db.Column(db.String(100))
+    is_right = db.Column(db.Integer)
+
+    def __init__(self, id_test, pilihan, is_right):
+        self.id_test = id_test
+        self.pilihan = pilihan
+        self.is_right = is_right
+
+
+# Schema Pilihan
+class PilihanTestSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'id_test', 'pilihan', 'is_right')
+
+
+# Init pilihan schema
+pilihan_test_schema = PilihanTestSchema()
+many_pilihan_test_schema = PilihanTestSchema(many=True)
+
+# Get All Pilihan Test
+@app.route('/pilihantest', methods=['GET'])
+def get_all_pilihan_test():
+    all_pilihan_test = PilihanTest.query.all()
+    result = many_pilihan_test_schema.dump(all_pilihan_test)
+
+    return jsonify(result)
+
+# Get All Kelas By id_test
+@app.route('/pilihantest/<id_test>', methods=['GET'])
+def get_all_pilihan_test_by_id_test(id_test):
+    all_pilihan_test = PilihanTest.query.filter_by(id_test=id_test)
+    result = many_pilihan_test_schema.dump(all_pilihan_test)
+
+    return jsonify(result)
+
+# Create a Pilihan Test
+@app.route('/pilihantest', methods=['POST'])
+def add_pilihan_test():
+    id_test = request.json['id_test']
+    pilihan = request.json['pilihan']
+    is_right = request.json['is_right']
+
+    new_pilihan_test = PilihanTest(id_test, pilihan, is_right)
+    db.session.add(new_pilihan_test)
+    db.session.commit()
+
+    return pilihan_test_schema.jsonify(new_pilihan_test)
+
+# Delete a pilihan Test
+@app.route('/pilihantest/<id>', methods=['DELETE'])
+def delete_pilihan_test(id):
+    pilihan_test = PilihanTest.query.get(id)
+    db.session.delete(pilihan_test)
+    db.session.commit()
+
+    return pilihan_test_schema.jsonify(pilihan_test)
+
+# Edit a Pilihan Test
+@app.route('/pilihantest/<id>', methods=['PUT'])
+def edit_pilihan_test(id):
+    pilihan_test = PilihanTest.query.get(id)
+
+    pilihan_test.id_test = request.json['id_test']
+    pilihan_test.pilihan = request.json['pilihan']
+    pilihan_test.is_right = request.json['is_right']
+
+    db.session.commit()
+
+    return pilihan_test_schema.jsonify(pilihan_test)
 
 
 # Run Server
